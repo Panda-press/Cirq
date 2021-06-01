434ddd09dfc500aad21f1c7417a20369b6aa1f08
Harvey <tyrrellhs@gmail.com>
2021-06-01 21:39:07 +0100

    Deutch Jozsa Algorithm Implemented

---
+++ Deutch_Jozsa.py
@@ -0,0 +1,24 @@
+import cirq
+from cirq import sim
+
+simulator = cirq.Simulator()
+q0, q1 = cirq.LineQubit.range(2)
+
+circuit = cirq.Circuit()
+circuit.append([cirq.X(q1), cirq.H(q0), cirq.H(q1)])
+
+#Start of function being tested
+
+circuit.append([cirq.CNOT(q0, q1)])
+# circuit.append([cirq.X(q1)])
+
+#End of function being tested
+
+circuit.append([cirq.H(q0)])
+circuit.append([cirq.measure(q0), cirq.measure(q0)])
+
+print(circuit)
+
+results = simulator.run(circuit, repetitions=10)
+
+print(results)
